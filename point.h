54126f361cace4f81ff1047fd57bc2f399103a63
SullyMak <91467801+SullyMak@users.noreply.github.com>
2021-12-03 11:50:04 +0100

    Add files via upload
    class Point + its functions

---
+++ point.h
@@ -0,0 +1,19 @@
+#ifndef POINT_H
+#define POINT_H
+class Point {
+public :
+	Point();
+	Point(float x, float y=2.);
+	void affiche (void) const;
+	static void affichePointRef (void);
+	void modPointRef (float x, float y);
+	void translate (float x, float y);
+	bool ok ();
+	float distance () const;
+    bool operator<(const Point&) const;
+    	float operator[](int n);
+private :
+	float xP, yP;
+	static Point PointRef;
+};
+#endif
