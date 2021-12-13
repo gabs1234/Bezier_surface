88b94a260cfed85d3afc9a640f1f50d4067e1295
Gabriel Lefloch <gabriel.lefloch@cea.fr>
2021-12-13 16:13:50 +0100

    changed file IO structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "data.hpp"
+#include "DataIO.hpp"
 #include <iostream>
 
 using namespace std;
@@ -9,17 +9,17 @@ TODO:
 		convert into barycentric points according to macro-triangle
 		if in triangle: find center of mass, and calculate coefficients of the 3 sub triangles
 
-HELLLO MOTHERFUCKER
 */
 
 int main(int argc, char const *argv[]) {
 	// Load initial control points that we wish to interpolate
-	DATA<float> control_points("data/hctr.pts", 2);
+	auto control_points = readData<float>("../data/hctr.pts", 2);
 	// Load triangulation
-	DATA<int> triangulation("data/hctr.tri", 32, 3);
+	auto triangulation = readData<int>("../data/hctr.tri", 32, 3);
 
-	control_points.print();
-	triangulation.print();
+	printData<float>(control_points);
+	printData<int>(triangulation);
+
 
 	return 0;
 }
