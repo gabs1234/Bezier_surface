6521d0ccaea19040280e3ed8d39c062f41a5918e
Sully <sully-mak@tutanota.com>
2021-12-22 18:43:40 +0100

    function update

--- src/func.cpp
+++ src/func.cpp
@@ -20,3 +20,14 @@ float ffuncdy(Point P){
 float gfunc(Point P){
 	return P[1]*P[1]*P[1]-2*P[1]*P[1]*P[0]-5*P[1]*P[0]*P[0]+10*P[1]*P[0]+1;
 }
+
+
+float gfuncdx(float x1, float x2)
+{
+    return (-2) * x2 * x2 - 10 * x1 * x2 + 10 * x2;
+}
+
+float gfundy(float x1, float x2)
+{
+    return 3 * x2 * x2 - 4 * x1 * x2 - 5 * x1 * x1 + 10 * x1;
+}
