d088d5ca546b8f6f9814809b11ac728b41198b15
Sully <sully-mak@tutanota.com>
2021-12-23 11:08:48 +0100

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,15 @@ using namespace std;
 
 
 float f(Point P){
-	return exp(P[0]+P[1]);	
+	return P[1]*P[1]*P[1]-2*P[1]*P[1]*P[0]-5*P[1]*P[0]*P[0]+10*P[1]*P[0]+1;
 }
 
 float dxf(Point P){
-	return exp(P[0]+P[1]);	
+	return (-2)*P[1]*P[1] - 10*P[0]*P[1] + 10*P[1];
 }
 
 float dyf(Point P){
-	return exp(P[0]+P[1]);	
+	return 3*P[1]*P[1] - 4*P[0]*P[1] - 5*P[0]*P[0] + 10*P[0];
 }
 
 int main(int argc, char const *argv[]) {
@@ -40,6 +40,9 @@ int main(int argc, char const *argv[]) {
 	vector<vector<float>> res( lines , std::vector<float> (columns));
 	
 	int id;
+	
+	int cur_id = 0;
+	int next_id;
 
 	Coefficients coef; 
 	vector<float> lambda(3);
@@ -47,16 +50,18 @@ int main(int argc, char const *argv[]) {
 	for( int i = 0; i < 31; i++ ){
 		for( int j = 0; j < 31; j++ ){
 			Point P(X[i][j], Y[i][j]);
-
+			
 			Triangle macro_triangle = findTriangle(P, triangulation);
 			Triangle micro_triangle = macro_triangle.findMicroTriangle(P);
-
-			calcCoefficient(macro_triangle, coef, f, dxf, dyf);
+			next_id = macro_triangle.getId();
+			if(next_id != cur_id){
+				calcCoefficient(macro_triangle, coef, f, dxf, dyf);} // check if we are still in the same triangle 
 
 			lambda = P.getBarycentric(micro_triangle);
 			id = micro_triangle.getId();
 
 			res[i][j] = computeInterpolation(id, lambda, coef);
+			cur_id = next_id;
 		}
 	}
 
